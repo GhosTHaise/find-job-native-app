03984161e00b02067fc371bfe57bd45fe0cc3d0c
GhosTHaise <Ghostrex2@gmail.com>
2023-03-22 21:51:01 +0300

    Craete Screen options

--- app/index.js
+++ app/index.js
@@ -1,12 +1,26 @@
-import { View , Text } from "react-native";
+import { useState } from "react";
+import { View , ScrollView,SafeAreaView, Text } from "react-native";
+import { Stack,useRouter } from "expo-router";
+
+import {COLORS,icons,images,SIZES} from "../constants"
+import {Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from "../components"
 
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>
+        <SafeAreaView style={{
+                flex : 1,
+                backgroundColor : COLORS.lightWhite
+            }}>
+            <Stack.Screen
+                options={{
+                    headerStyle : { backgroundColor : COLORS.lightWhite},
+                    headerShadowVisible : false
+                }}
+            >
                 Home
-            </Text>
-        </View>
+            </Stack.Screen>
+        </SafeAreaView>
     )
 }
 
