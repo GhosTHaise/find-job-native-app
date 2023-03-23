5d9446e0fdd5ebfa2ada498270e46442c54b3c9f
GhosTHaise <Ghostrex2@gmail.com>
2023-03-23 20:26:41 +0300

    Load font

--- app/_layout.js
+++ app/_layout.js
@@ -3,8 +3,24 @@ import { useCallback } from "react";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
 const Layout = () => {
-    return <Stack />
+    const [fontsLoaded] = useFonts({
+        DMBold : require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular : require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    const onLayoutRootView = useCallback(async()=>{
+        if(fontsLoaded){
+            await SplashScreen.hideAsync()
+        }
+
+        if(!fontsLoaded) return null;
+
+    },[fontsLoaded])
+
+    return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout;
