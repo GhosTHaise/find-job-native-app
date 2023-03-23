33e9773969f892a9019b055b8cc2418af9a6af5d
GhosTHaise <Ghostrex2@gmail.com>
2023-03-23 20:09:11 +0300

    Create welcome component

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,7 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 const Layout = () => {
     return <Stack />
