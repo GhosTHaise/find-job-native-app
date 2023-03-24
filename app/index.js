087d1ef5b52a07410f01b21515bcd4ba08a2cc45
GhosTHaise <Ghostrex2@gmail.com>
2023-03-24 20:33:00 +0300

    Create Popular job slide flatlist

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { View , ScrollView,SafeAreaView, Text } from "react-native";
+import { 
+    View , 
+    ScrollView,
+    SafeAreaView
+    , Text } from "react-native";
 import { Stack,useRouter } from "expo-router";
 
 import {COLORS,icons,images,SIZES} from "../constants"
