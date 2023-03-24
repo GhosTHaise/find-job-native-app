19e05083c62832a596e11e300d1853fae8ed102a
GhosTHaise <Ghostrex2@gmail.com>
2023-03-24 21:05:19 +0300

    create useFetch to retrieve data from api

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,12 +11,12 @@ import {
 import styles from './popularjobs.style'
 import { COLORS,SIZES } from '../../../constants'
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import userFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
   const router = useRouter();
 
-  const isLoading = false;
-  const error = false;
+  
   return (
     <View style={styles.container}>
       <View style={styles.header} >
---
+++ hook/useFetch.js
@@ -0,0 +1,46 @@
+import {useState,useEffect} from "react"
+import axios from "axios"
+import {RAPID_API_KEY} from "@env";
+
+const rapidAPiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint,query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: {...query},
+        headers: {
+          'X-RapidAPI-Key': rapidAPiKey,
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+      };
+      
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options)
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert("There is an error")
+        }finally{
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(()=> {
+        fetchData();
+    },[])
+
+    const refetch = () =>{
+        setIsLoading(true);
+        fetchData();
+    }
+    return {data,isLoading,error,refetch}
+};
+
