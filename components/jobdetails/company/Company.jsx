ff79298da61de30b89cc65e765a07a431ccbee43
GhosTHaise <Ghostrex2@gmail.com>
2023-03-27 20:57:16 +0300

    Build company details page

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -8,8 +8,37 @@ const Company = ({companyLogo,jobTitle,companyName,Location}) => {
   return (
     <View style={styles.container}>
       <View style={styles.logoBox}>
-
+         <Image
+            source={{
+              uri : checkImageURL(companyLogo ) 
+              ? 
+                companyLogo 
+              : 
+                "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
+            }}
+            style={styles.logoImage}
+         />
       </View>
+
+      <View style={styles.jobTitleBox}>
+            <Text styles={styles.jobTitle}>
+              {jobTitle}
+            </Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+            <Text style={styles.companyName}>
+                {companyName} /
+            </Text>
+            <View style={styles.locationBox}>
+                <Image
+                  source={icons.location}
+                  resizeMode="contain"
+                  style={styles.locationImage}
+                />
+                <Text styles={styles.locationName}>{Location}</Text>
+            </View>
+      </View>     
     </View>
   )
 }
